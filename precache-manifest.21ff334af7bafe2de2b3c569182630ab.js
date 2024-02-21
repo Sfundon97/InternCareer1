d6487e59c88b8ce3702cb118c65f14bf010bc4f0
Sfundo <sfundon97@gmail.com>
2024-02-21 15:29:11 +0200

    Updates

---
+++ precache-manifest.21ff334af7bafe2de2b3c569182630ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0357c60bf6b8f8f690a6c31d52d13561",
+    "url": "/InternCareer1/index.html"
+  },
+  {
+    "revision": "2569f4cf344ffd92f8cf",
+    "url": "/InternCareer1/static/css/main.439524be.chunk.css"
+  },
+  {
+    "revision": "993887c91eba5f85b656",
+    "url": "/InternCareer1/static/js/2.83fde0ba.chunk.js"
+  },
+  {
+    "revision": "33c9af1c367bdbf7cda8de087498f4d5",
+    "url": "/InternCareer1/static/js/2.83fde0ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2569f4cf344ffd92f8cf",
+    "url": "/InternCareer1/static/js/main.d8320ad1.chunk.js"
+  },
+  {
+    "revision": "a322f62c5f3c43cb6052",
+    "url": "/InternCareer1/static/js/runtime-main.045ee4f4.js"
+  }
+]);
